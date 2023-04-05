4fe4eeba3fcdf980e2bed018f246463252ed72b8
ArtemDan <artem.danylenko1991@gmail.com>
2023-04-05 22:03:08 +0300

    add changes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sl2fua&(-86@f48jk&_t+8tlio09$@8_jk-t78t$u^5dz3y3-f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'women.apps.WomenConfig'
 ]
 
 MIDDLEWARE = [
--- myproject/women/views.py
+++ myproject/women/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Сторінка додатку women")
+
+def categories(request, id):
+    if (request.GET):
+        print(request.GET)
+    return HttpResponse(f"<h1>Статті по категоріям</h1><p>{id}</p>")
+
+def archive(request, year):
+    if int(year) > 2023:
+        return redirect('home', permanent=True)
+    return HttpResponse(f"<h1>Статті по категоріям</h1><p>{year}</p>")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Сторінка не знайдена</h1>')
