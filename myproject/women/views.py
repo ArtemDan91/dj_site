a733db2488a0c7abd9caea31edb2c150c1b99a98
ArtemDan <artem.danylenko1991@gmail.com>
2023-04-06 22:19:20 +0300

    add changes

--- myproject/women/views.py
+++ myproject/women/views.py
@@ -12,10 +12,13 @@ menu = [{'title': "Головна Сторінка", 'url_name': 'home'},
 
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
     context = {
         'posts': posts,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Головна сторінка',
+        'cats': cats,
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
 
@@ -33,6 +36,23 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f"Відображення статті з id = {post_id}")
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Відображення по рубрикам',
+        'cats': cats,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
+
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Сторінка не знайдена</h1>')
