3dfe8c7e6c124239f52fcd5fbe5f0eda711e5fe7
ArtemDan <artem.danylenko1991@gmail.com>
2023-04-07 23:58:52 +0300

    add changes

--- myproject/women/views.py
+++ myproject/women/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 
+from .forms import *
 from .models import *
 
 menu = [{'title': "Головна Сторінка", 'url_name': 'home'},
@@ -24,7 +25,18 @@ def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'Про сайт'})
 
 def addpage(request):
-    return HttpResponse("Додання статті")
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Помилка додавання посту')
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form':form, 'menu': menu, 'title': 'Додавання статті'})
+
 
 def contact(request):
     return HttpResponse("Зворотній зв'язок")
@@ -32,8 +44,8 @@ def contact(request):
 def login(request):
     return HttpResponse("Авторизація")
 
-def show_post(request, post_id):
-    post = get_object_or_404(Women, pk=post_id)
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
     context = {
         'post': post,
         'menu': menu,
@@ -43,8 +55,8 @@ def show_post(request, post_id):
     return render(request, 'women/post.html', context=context)
 
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+def show_category(request, cat_slug):
+    posts = Women.objects.filter(cat__slug=cat_slug)
 
     if len(posts) == 0:
         raise Http404()
@@ -53,7 +65,7 @@ def show_category(request, cat_id):
         'posts': posts,
         'menu': menu,
         'title': 'Відображення по рубрикам',
-        'cat_selected': cat_id,
+        'cat_selected': cat_slug,
     }
     return render(request, 'women/index.html', context=context)
 
