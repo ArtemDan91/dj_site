fac21472964f94489c262a8d000d5f53a0f0e1fb
ArtemDan <artem.danylenko1991@gmail.com>
2023-04-19 18:51:30 +0300

    add changes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'captcha',
     'women.apps.WomenConfig',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -151,5 +153,11 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
